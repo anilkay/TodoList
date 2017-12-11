b3162f7e3ec36213541f58253177876c25694e8b
Anıl Kaynar <aanilkay@gmail.com>
2017-12-11 22:20:05 +0300

    Add Adapter

---
+++ app/src/main/java/com/anilkaynar/todolist/TodoAdapter.java
@@ -0,0 +1,56 @@
+package com.anilkaynar.todolist;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by anilkaynar on 11.12.2017.
+ */
+
+public class TodoAdapter extends BaseAdapter {
+    ArrayList<ToDo> toDoArrayList;
+    LayoutInflater layoutInflater;
+
+    TodoAdapter(Activity activity, ArrayList<ToDo> arrayList) {
+        layoutInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        toDoArrayList = arrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View satir;
+        ToDo to = toDoArrayList.get(i);
+        satir = layoutInflater.inflate(R.layout.todo_line, null);
+        CheckBox c = satir.findViewById(R.id.cekbox);
+        TextView content = satir.findViewById(R.id.content1);
+        TextView tarih = satir.findViewById(R.id.tarih);
+        TextView saat = satir.findViewById(R.id.saat);
+        content.setText(to.metin);
+        tarih.setText(to.tarih);
+        saat.setText(to.zaman);
+        return satir;
+    }
+}
