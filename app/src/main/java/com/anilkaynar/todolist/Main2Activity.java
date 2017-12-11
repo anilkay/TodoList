1a476bcd7c5203dca5f2790d8da522ec7663553c
Anıl Kaynar <aanilkay@gmail.com>
2017-12-11 22:44:46 +0300

    Add some Logic

--- app/src/main/java/com/anilkaynar/todolist/Main2Activity.java
+++ app/src/main/java/com/anilkaynar/todolist/Main2Activity.java
@@ -1,8 +1,8 @@
 package com.anilkaynar.todolist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -29,8 +29,8 @@ public class Main2Activity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivityForResult(new Intent(getApplicationContext(), AddTodo.class), 1);
+
             }
         });
     }
